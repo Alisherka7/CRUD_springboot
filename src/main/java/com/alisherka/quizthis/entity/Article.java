d24d2dcf7d4cb2e8ff6d575f19f9c87030d966a5
Alisherka7 <alisherka7@jj.ac.kr>
2022-09-23 21:10:22 +0900

    rest api

--- src/main/java/com/alisherka/quizthis/entity/Article.java
+++ src/main/java/com/alisherka/quizthis/entity/Article.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity // DB가 해당 객체를 인식 가능
@@ -18,11 +15,20 @@ import javax.persistence.Id;
 @Getter
 public class Article {
     @Id
-    @GeneratedValue // 1,2,3,4.... 자동 생성 annotation
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id를 자동 생성 어노테이션
     private Long id;
 
     @Column
     private String title;
     @Column
     private String content;
+
+    public void patch(Article article) {
+        if(article.title != null){
+            this.title = article.title;
+        }
+        if(article.content != null){
+            this.content = article.content;
+        }
+    }
 }
